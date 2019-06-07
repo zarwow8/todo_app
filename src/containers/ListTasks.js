a4c6d1b835b28823388eae08e47b978baa218704
Ivan <zarwow8@gmail.com>
2019-06-07 09:55:10 +0300

    create and add ListTasks  container

---
+++ src/containers/ListTasks.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Task from "../components/Task";
+
+const List = () => {
+  const tasks = [
+    {
+      id: 1,
+      title: "Lorem ipsum",
+      desc: "Duis aute irure dolor in reprehenderit in voluptate velit"
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum",
+      desc: "Duis aute irure dolor in reprehenderit in voluptate velit"
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum",
+      desc: "Duis aute irure dolor in reprehenderit in voluptate velit"
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum",
+      desc: "Duis aute irure dolor in reprehenderit in voluptate velit"
+    },
+    {
+      id: 5,
+      title: "Lorem ipsum",
+      desc: "Duis aute irure dolor in reprehenderit in voluptate velit"
+    }
+  ];
+
+  return (
+    <ul>
+      {tasks.map(item => (
+        <Task key={item.id} taskItem={item} />
+      ))}
+    </ul>
+  );
+};
